cddea87bb2337d993fe39c66f15f34f8514d5fdb
DMoncayoGit <123705189+DMoncayoGit@users.noreply.github.com>
2026-07-10 09:50:17 -0500

    Update 03_analisis_variables_predictivas.py

--- Caso Práctico 1.2/03_analisis_variables_predictivas.py
+++ Caso Práctico 1.2/03_analisis_variables_predictivas.py
@@ -10,9 +10,24 @@ correlacion = df.corr( numeric_only=True )
 
 print( correlacion[ "deserto_bin" ].sort_values( ascending=False))
 
-plt.figure( figsize=(10,7) )
+plt.figure( figsize=(16,12) )
 
-sns.heatmap( correlacion, annot=True )
+#sns.heatmap( correlacion, annot=True )
+
+sns.heatmap(
+    correlacion,
+    annot=True,                # muestra los números
+    annot_kws={"size":14},     # tamaño de los números
+    #cmap="coolwarm",          # paleta de colores opcional
+    fmt=".4f"                  # formato de los números (2 decimales)
+)
+
+
+plt.xticks(rotation=45, ha="right")
+plt.yticks(rotation=0)
+
+plt.tight_layout()
+plt.subplots_adjust(bottom=0.2, left=0.2) 
 
 plt.title( "Matriz de correlación" )
 
